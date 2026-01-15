8d78e83a9e80672bc4581dfc306dcb549b7be847
Si Fad <sifayed00@gmail.com>
2026-01-15 23:01:58 +0600

    CHANGES: Added landing section animation.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import 'remixicon/fonts/remixicon.css';
 
 export default function App() {
     let [showContent, setShowContent] = useState(false);
+
+    // First intro animation
     useGSAP(() => {
         const tl = gsap.timeline();
 
@@ -30,7 +32,24 @@ export default function App() {
         });
     });
 
-    const image = 'https://ibb.co.com/bgM9Y3hH';
+    // animation
+    useGSAP(() => {
+        const main = document.querySelector('.main');
+
+        main?.addEventListener('mousemove', function (e) {
+            const XMove = (e.clientX / window.innerWidth - 0.5) * 40;
+
+            gsap.to('.main .text', {
+                x: `${XMove * 0.4}%`,
+            });
+            gsap.to('.sky', {
+                x: XMove,
+            });
+            gsap.to('.bg', {
+                x: XMove * 0.7  ,
+            });
+        });
+    }, [showContent]);
 
     return (
         <>
@@ -81,33 +100,45 @@ export default function App() {
                         </div>
                         <div className="imagediv relative overflow-hidden w-full h-screen">
                             <img
-                                className="absolute top-0 left-0 w-full h-full object-cover"
+                                className="sky absolute top-0 left-0 w-full h-full scale-[1.3] object-cover"
                                 src="./sky.png"
                                 alt=""
                             ></img>
                             <img
-                                className="absolute top-0 left-0 w-full h-full object-cover"
+                                className="bg absolute top-0 left-0 w-full h-full scale-[1.2] object-cover"
                                 src="./bg.png"
                                 alt=""
                             ></img>
-                            <div className='text absolute flex flex-col gap-3 top-20 left-1/2 -translate-x-1/2 text-white'>
-                                <h3 className='text-[12rem] leading-none -ml-40'>grand</h3>
-                                <h3 className='text-[12rem] leading-none -ml-20'>theft</h3>
-                                <h3 className='text-[12rem] leading-none -ml-40'>auto</h3>
+                            <div className="text absolute flex flex-col gap-3 top-20 left-1/2 -translate-x-1/2 text-white">
+                                <h3 className="text-[12rem] leading-none -ml-40">
+                                    grand
+                                </h3>
+                                <h3 className="text-[12rem] leading-none -ml-20">
+                                    theft
+                                </h3>
+                                <h3 className="text-[12rem] leading-none -ml-40">
+                                    auto
+                                </h3>
                             </div>
                             <img
-                                className="absolute -bottom-[25%] left-1/2 -translate-x-1/2 scale-[1.4]"
+                                className="character absolute -bottom-[25%] left-1/2 -translate-x-1/2 scale-[1.4]"
                                 src="./girlbg.png"
                                 alt=""
                             ></img>
                         </div>
                         <div className="btmbar absolute bottom-0 left-0 text-white w-full py-16 px-10 bg-linear-to-t from-black to-transparent">
                             <div className="flex gap-4 items-center">
-                                <i class="text-4xl ri-arrow-down-line"></i>
-                                <h3 className='text-2xl font-[Helvetica_Now_Display]'>Scroll Down</h3>
+                                <i className="text-4xl ri-arrow-down-line"></i>
+                                <h3 className="text-2xl font-[Helvetica_Now_Display]">
+                                    Scroll Down
+                                </h3>
                             </div>
 
-                            <img className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-22' src="./ps5.png" alt="" />
+                            <img
+                                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-22"
+                                src="./ps5.png"
+                                alt=""
+                            />
                         </div>
                     </div>
                 </div>
