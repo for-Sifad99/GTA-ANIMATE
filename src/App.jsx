974c9581018b74d11c5961d0d8ecb00db4732951
Si Fad <sifayed00@gmail.com>
2026-01-15 20:06:12 +0600

    CHANGES: Added gsap and applyed some animation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 export default function App() {
+    useGSAP(() => {
+        const tl = gsap.timeline();
+
+        tl.to('.vi-mask-group', {
+            rotate: 10,
+            duration: 2,
+            ease: 'Power4.easeInOut',
+            transformOrigin: '50% 50%',
+        })
+        .to('.vi-mask-group',{
+            scale: 10,
+            duration: 2,
+            delay: -1.8,
+            ease: 'Expo.easeInOut',
+            transformOrigin: '50% 50%',
+            opacity: 0,
+        })
+    });
+
     return (
         <>
             <div className="svg flex items-center justify-center fixed top-0 left-0 z-50 w-full h-screen overflow-hidden bg-black">
