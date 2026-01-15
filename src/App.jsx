64b6b1237ef9bcad100e59cae463300c5ec60c35
Si Fad <sifayed00@gmail.com>
2026-01-15 22:25:46 +0600

    CHANGES: Added landing page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import 'remixicon/fonts/remixicon.css';
 
 export default function App() {
     let [showContent, setShowContent] = useState(false);
@@ -29,6 +30,8 @@ export default function App() {
         });
     });
 
+    const image = 'https://ibb.co.com/bgM9Y3hH';
+
     return (
         <>
             <div className="svg flex items-center justify-center fixed top-0 left-0 z-50 w-full h-screen overflow-hidden bg-black">
@@ -52,7 +55,7 @@ export default function App() {
                         </mask>
                     </defs>
                     <image
-                        href="/bg.png"
+                        href="./bg.png"
                         width="100%"
                         height="100%"
                         preserveAspectRatio="xMidYMid slice"
@@ -60,6 +63,55 @@ export default function App() {
                     />
                 </svg>
             </div>
+
+            {showContent && (
+                <div className="main w-full">
+                    <div className="landing w-full h-screen bg-black">
+                        <div className="navbar absolute z-10 top-0 left-0 w-full py-16 px-16">
+                            <div className="logo flex gap-6">
+                                <div className="lines flex flex-col gap-2">
+                                    <div className="line w-15 h-1.5 bg-white"></div>
+                                    <div className="line w-10 h-1.5 bg-white"></div>
+                                    <div className="line w-5 h-1.5 bg-white"></div>
+                                </div>
+                                <h3 className="text-4xl leading-none -mt-2.25 text-white">
+                                    Rockstar
+                                </h3>
+                            </div>
+                        </div>
+                        <div className="imagediv relative overflow-hidden w-full h-screen">
+                            <img
+                                className="absolute top-0 left-0 w-full h-full object-cover"
+                                src="./sky.png"
+                                alt=""
+                            ></img>
+                            <img
+                                className="absolute top-0 left-0 w-full h-full object-cover"
+                                src="./bg.png"
+                                alt=""
+                            ></img>
+                            <div className='text absolute flex flex-col gap-3 top-20 left-1/2 -translate-x-1/2 text-white'>
+                                <h3 className='text-[12rem] leading-none -ml-40'>grand</h3>
+                                <h3 className='text-[12rem] leading-none -ml-20'>theft</h3>
+                                <h3 className='text-[12rem] leading-none -ml-40'>auto</h3>
+                            </div>
+                            <img
+                                className="absolute -bottom-[25%] left-1/2 -translate-x-1/2 scale-[1.4]"
+                                src="./girlbg.png"
+                                alt=""
+                            ></img>
+                        </div>
+                        <div className="btmbar absolute bottom-0 left-0 text-white w-full py-16 px-10 bg-linear-to-t from-black to-transparent">
+                            <div className="flex gap-4 items-center">
+                                <i class="text-4xl ri-arrow-down-line"></i>
+                                <h3 className='text-2xl font-[Helvetica_Now_Display]'>Scroll Down</h3>
+                            </div>
+
+                            <img className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-22' src="./ps5.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
