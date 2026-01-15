74e96c6388038f1a8f12e29a86b67ec0342c53be
Si Fad <sifayed00@gmail.com>
2026-01-15 18:57:43 +0600

    CHNAGES: Tailwind setup.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div>
+            <h1 className='text-5xl font-bold text-blue-600 text-center mt-10'>Hello GTA!</h1>
+        </div>
+    );
+}
